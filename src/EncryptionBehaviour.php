6d75c083bdf4c244fe8a58d918cbb324dc77311f
Max <max@frieze.com>
2018-05-23 20:58:04 +0100

    ..

--- src/EncryptionBehaviour.php
+++ src/EncryptionBehaviour.php
@@ -6,11 +6,15 @@
  */
 class EncryptionBehaviour extends CActiveRecordBehavior
 {
-    private $_owner;
     public $encryptedFields = array();
+
+    private $_security;
+    private $_owner;
+    private $_preCheck = 'XN%_'; //Used when checking if string is already encrypted
 
     public function __construct()
     {
+        $this->_security = Yii::app()->getSecurityManager();
         $this->_owner = $this->getOwner();
     }
 
@@ -20,9 +24,10 @@ class EncryptionBehaviour extends CActiveRecordBehavior
      */
     public function beforeSave($event) {
         foreach($this->encryptedFields as $encryptedField){
+            $value = $this->owner->{$encryptedField};
             //Protect against double encryption
-            if ($this->owner->{$encryptedField} && @!Yii::app()->getSecurityManager()->decrypt(base64_decode($this->owner->{$encryptedField})))
-                $this->owner->{$encryptedField} = base64_encode(Yii::app()->getSecurityManager()->encrypt($this->owner->{$encryptedField}));
+            if ($value && !$this->_isEncrypted($value))
+                $this->owner->{$encryptedField} = $this->_encrypt($value);
         }
     }
 
@@ -32,8 +37,9 @@ class EncryptionBehaviour extends CActiveRecordBehavior
      */
     public function afterSave($event){
         foreach($this->encryptedFields as $encryptedField){
-            if($this->owner->{$encryptedField})
-                $this->owner->{$encryptedField} = Yii::app()->getSecurityManager()->decrypt(base64_decode($this->owner->{$encryptedField}));
+            $value = $this->owner->{$encryptedField};
+            if($value)
+                $this->owner->{$encryptedField} = $this->_decrypt($value);
         }
     }
 
@@ -43,8 +49,40 @@ class EncryptionBehaviour extends CActiveRecordBehavior
      */
     public function afterFind($event) {
         foreach($this->encryptedFields as $encryptedField){
-            if ($this->owner->{$encryptedField} && @Yii::app()->getSecurityManager()->decrypt(base64_decode($this->owner->{$encryptedField})))
-                $this->owner->{$encryptedField} = Yii::app()->getSecurityManager()->decrypt(base64_decode($this->owner->{$encryptedField}));
+            $value = $this->owner->{$encryptedField};
+            if ($value && $this->_isEncrypted($value))
+                $this->owner->{$encryptedField} = $this->_decrypt($value);
         }
     }
+
+    /**
+     * @param $field
+     * @return bool
+     */
+    private function _isEncrypted($field){
+        $decrypted = $this->_decrypt($field, true);
+        if (substr($decrypted, 0, strlen($this->_preCheck)) === $this->_preCheck)
+            return true;
+        else return false;
+    }
+
+    /**
+     * @param $field
+     * @param bool $keepPreCheck
+     * @return bool|string
+     */
+    private function _decrypt($field, $keepPreCheck=false){
+        if($keepPreCheck)
+            return $this->_security->decrypt(base64_decode($field));
+        else return substr($this->_security->decrypt(base64_decode($field)), strlen($this->_preCheck));
+    }
+
+    /**
+     * @param $field
+     * @return string
+     */
+    private function _encrypt($field){
+        return base64_encode($this->_security->encrypt($this->_preCheck . $field));
+    }
+
 }
